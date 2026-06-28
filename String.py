a11d225ab39f4161a9ebd68c7e87af7aa1848cb4
Mansi Ingale <ingalemansi70@gmail.com>
2026-06-28 21:14:08 +0530

    Function is called with user entered string and empty string condition is handeled

--- String.py
+++ String.py
@@ -1,14 +1,16 @@
 def analyze_string(s):
-    print("The length of string is : ",len(s))
-    print("The reverse of string is : ",s[: : -1])
-    count = 0
-    for i in range(len(s)):
-        if i in ("a","e","i","o","u") :
-            count+=1
-    print("The number of vowels in the string are : ",count)
-    print("Characters of string with their positive and negative index are : ")
-    for i in range(len(s)):
-        print("Positive index of ",s[i], " is :",i," Negative index is : ",i - len(s))
+    if s :
+        print("The length of string is : ",len(s))
+        print("The reverse of string is : ",s[: : -1])
+        count = 0
+        for i in range(len(s)):
+            if i in ("a","e","i","o","u") :
+                count+=1
+        print("The number of vowels in the string are : ",count)
+        print("Characters of string with their positive and negative index are : ")
+        for i in range(len(s)):
+            print("Positive index of ",s[i], " is :",i," Negative index is : ",i - len(s))
 
-analyze_string("Mansi")
+s = input("Enter a string : ")
+analyze_string(s)
 
