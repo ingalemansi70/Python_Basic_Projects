d6e1b7ed1028a208b6846bded73ce38c00a5c6ef
Mansi Ingale <ingalemansi70@gmail.com>
2026-06-28 21:09:43 +0530

    characters printed with their positive and negativ index

--- String.py
+++ String.py
@@ -5,6 +5,10 @@ def analyze_string(s):
     for i in range(len(s)):
         if i in ("a","e","i","o","u") :
             count+=1
-
     print("The number of vowels in the string are : ",count)
+    print("Characters of string with their positive and negative index are : ")
+    for i in range(len(s)):
+        print("Positive index of ",s[i], " is :",i," Negative index is : ",i - len(s))
+
 analyze_string("Mansi")
+
