243715a7a415dc80777fcb2a287f772eedc88024
Mansi Ingale <ingalemansi70@gmail.com>
2026-06-28 21:21:46 +0530

    5 subject marks taken and ValueError handeled

--- String.py
+++ String.py
@@ -1,3 +1,4 @@
+#Question 2)
 def analyze_string(s):
     if s :
         print("The length of string is : ",len(s))
@@ -10,7 +11,21 @@ def analyze_string(s):
         print("Characters of string with their positive and negative index are : ")
         for i in range(len(s)):
             print("Positive index of ",s[i], " is :",i," Negative index is : ",i - len(s))
-
+    else :
+        print("String is empty!")
 s = input("Enter a string : ")
 analyze_string(s)
+
+#Question 3)
+
+def manage_marks():
+    s1 = int(input("Enter the marks of sub1 : "))
+    s2 = int(input("Enter the marks of sub2 : "))
+    s3 = int(input("Enter the marks of sub3 : "))
+    s4 = int(input("Enter the marks of sub4 : "))
+    s5 = int(input("Enter the marks of sub5 : "))
+
+
+
+
 
